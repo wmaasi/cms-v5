bf6a122817ed03e80077bd5c1d2cf535b85f4dbe
William Maas <william.maas@4thwave.solutions>
2026-06-02 16:26:41 -0600

    Fix: add POST method to entity sync route

--- src/api/entity/routes/entity-sync.js
+++ src/api/entity/routes/entity-sync.js
@@ -1,10 +1,17 @@
-// src/api/entity/routes/entity-sync.js
 'use strict';
-
 module.exports = {
   routes: [
     {
       method: 'GET',
+      path: '/entity/sync',
+      handler: 'entity.sync',
+      config: {
+        auth: false,
+        policies: [],
+      },
+    },
+    {
+      method: 'POST',
       path: '/entity/sync',
       handler: 'entity.sync',
       config: {
